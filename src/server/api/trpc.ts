ec0966ee938231fcc12d236e46ac7f3a5d1cc355
Uğur Oruç <uoruc5@hotmail.com>
2025-10-31 09:10:46 +0100

    add: initial run check

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -12,8 +12,6 @@ import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import superjson from "superjson";
 import { ZodError } from "zod";
 
-import { db } from "~/server/db";
-
 /**
  * 1. CONTEXT
  *
@@ -35,9 +33,7 @@ interface CreateContextOptions {}
  * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
  */
 const createInnerTRPCContext = (_: CreateContextOptions) => {
-  return {
-    db,
-  };
+  return {};
 };
 
 /**
